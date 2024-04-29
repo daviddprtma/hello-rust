08510c0e2958a411ebb418e4b83f27f70455b052
daviddprtma <davidchristianpratama@gmail.com>
2024-04-29 11:47:37 +0700

    learning tour semantic rust  quest 2 done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let mut even_numbers: Vec<i32> = Vec::new();
+    let max_value = 300;
+
+    for number in 1..=max_value{
+        if number % 2 == 0 {
+            println!("{}", max_value - number);
+            even_numbers.push(number);
+        }
+    }
+
+    println!("{:?}", even_numbers.iter().rev().collect::<Vec<_>>());
+
 }
