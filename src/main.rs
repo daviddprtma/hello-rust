b215a31300d60814a5f7fd7ae6c37466716bf7d7
daviddprtma <davidchristianpratama@gmail.com>
2024-05-01 18:56:06 +0700

    quest 3 done for learning important core concepts of rust

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,57 @@
-fn main() {
-    let mut even_numbers: Vec<i32> = Vec::new();
-    let max_value = 300;
+use std::f64::consts::PI;
 
-    for number in 1..=max_value{
-        if number % 2 == 0 {
-            println!("{}", max_value - number);
-            even_numbers.push(number);
+enum Shape<T> {
+    Circle(T),
+    Triangle(T, T),
+    Rectangle(T, T),
+}
+
+impl<T: Into<f64> + Copy> Shape<T> {
+    fn area(&self) -> f64 {
+        match self {
+            Shape::Circle(diameter) => {
+                let radius = (*diameter).into() / 2.0;
+                PI * radius.powf(2.0)
+            }
+            Shape::Triangle(base, height) => {
+                let base_f64: f64 = (*base).into();
+                let height_f64: f64 = (*height).into();
+                (base_f64 * height_f64) / 2.0
+            }
+            Shape::Rectangle(width, length) => {
+                let width_f64: f64 = (*width).into();
+                let length_f64: f64 = (*length).into();
+                width_f64 * length_f64
+            }
         }
     }
+}
 
-    println!("{:?}", even_numbers.iter().rev().collect::<Vec<_>>());
+fn main() {
+    let base: u8 = 24;
+    let height: u8 = 24;
+    let triangle = Shape::Triangle(base, height);
+    let triangle_area = triangle.area();
 
+    let width: u8 = 12;
+    let length: u8 = 24;
+    let rectangle = Shape::Rectangle(width, length);
+    let rectangular_area = rectangle.area();
+
+    let diameter: u8 = 45;
+    let circle = Shape::Circle(diameter);
+    let circle_area = circle.area();
+
+    println!(
+        "The area of the triangle with a base of {} and a height of {} is {:.5}",
+        base, height, triangle_area
+    );
+    println!(
+        "The area of the rectangle with a width of {} and a length of {} is {:.5}",
+        width, length, rectangular_area
+    );
+    println!(
+        "The area of the circle with a diameter of {} is {:.5}",
+        diameter, circle_area
+    );
 }
